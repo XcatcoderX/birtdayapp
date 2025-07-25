883a5fbfb8f84bdcbe175ae5e0a1b4bd9bcd000f
Piyali Jina <piyallijina@gmail.com>
2025-07-25 09:59:42 -0700

    Initial Commit

---
+++ birtdayapp/birtdayappApp.swift
@@ -0,0 +1,17 @@
+//
+//  birtdayappApp.swift
+//  birtdayapp
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct birtdayappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
