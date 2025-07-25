3aac18d66da87a23bd1aef7058052418cae62114
Piyali Jina <piyallijina@gmail.com>
2025-07-25 10:44:04 -0700

    it works now and i can add ppl and days :3

--- birtdayapp/ContentView.swift
+++ birtdayapp/ContentView.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends : [Friend] = [Friend(name: "Aditi", birthday: .now), Friend(name: "Piyali", birthday:.now)]
+        
+    @State private var newBirthday = Date.now
+    @State private var newName = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format:
+                            .dateTime.month(.wide).day()
+                            .year())
+                }
+                
+                
+                
+                
+                
+                
+                
+            }//list
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom){
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New birthday")
+                        .font(.headline)
+                   
+                    
+                    DatePicker(selection: $newBirthday){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                       
+                    }//datepicker
+                    
+                    
+                    Button("Save"){
+                        let newFriend = Friend(name: newName,
+                                birthday: newBirthday)
+                        friends.append(newFriend)
+                        
+                    }//button
+                }//vstack
+                
+                .padding()
+                .background(.bar)
+            
+            }//safeareainset
+        }//Navstack
+    }//body
+}//struct
 
 #Preview {
     ContentView()
-}
+}//preview
